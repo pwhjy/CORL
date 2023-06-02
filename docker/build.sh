e7f55d11b5e0f5ec3d04156a5141c8a31425aac4
pwhjy <1249277185@qq.com>
2023-06-02 16:22:41 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 DOCKER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOCAL_DIR="$(cd "(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
-docker build -t offlinerl/offlinerl_dev --build-arg -f "${DOCKER_PATH}/dev"
+docker build -t corl/corl_dev --build-arg -f "${DOCKER_PATH}/dev"
